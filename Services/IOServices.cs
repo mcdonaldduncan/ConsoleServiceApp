74fcf938beb9d1d59c97792725366cf2f3d25861
mcdonaldduncan <380939@colum.edu>
2023-02-07 14:49:47 -0600

    updated summaries

--- Services/IOServices.cs
+++ Services/IOServices.cs
@@ -8,6 +8,9 @@ namespace ConsoleServiceApp.Services
 {
     internal class IOServices
     {
+        /// <summary>
+        /// Get user input and parse to double, recursively fall back if parse fails
+        /// </summary>
         public double GetInputToDouble()
         {
             if (double.TryParse(Console.ReadLine(), out double input))
@@ -20,12 +23,20 @@ namespace ConsoleServiceApp.Services
             }
         }
 
-        public string FormatAppResults(double sum, double dif, double prod, double quot)
+        /// <summary>
+        /// Format Sum, Difference, Product and Quotient for display
+        /// </summary>
+        /// <param name="sum">sum result</param>
+        /// <param name="difference">difference result</param>
+        /// <param name="product">product result</param>
+        /// <param name="quotient">quotient result</param>
+        /// <returns></returns>
+        public string FormatAppResults(double sum, double difference, double product, double quotient)
         {
             return $"Sum: {sum}\n" +
-                $"Difference: {dif}\n" +
-                $"Product: {prod}\n" +
-                $"Quotient: {quot}";
+                $"Difference: {difference}\n" +
+                $"Product: {product}\n" +
+                $"Quotient: {quotient}";
         }
     }
 }
--- Services/MathServices.cs
+++ Services/MathServices.cs
@@ -8,21 +8,33 @@ namespace ConsoleServiceApp.Services
 {
     internal class MathServices
     {
+        /// <summary>
+        /// Returns sum of a + b
+        /// </summary>
         public double Sum(double a, double b)
         {
             return a + b;
         }
 
+        /// <summary>
+        /// Returns difference of a - b
+        /// </summary>
         public double Difference(double a, double b)
         {
             return a - b;
         }
 
+        /// <summary>
+        /// Returns product of a * b
+        /// </summary>
         public double Product(double a, double b)
         {
             return a * b;
         }
 
+        /// <summary>
+        /// Returns quotient of a / b
+        /// </summary>
         public double Quotient(double a, double b)
         {
             return a / b;
